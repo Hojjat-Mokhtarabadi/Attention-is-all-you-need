21a200726631da9ea0a8dca40258f908338ce537
Hojjat Mokhtarabadi <51716184+Hojjat-Mokhtarabadi@users.noreply.github.com>
2022-02-02 11:01:49 +0330

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-python main.py --save --heads 4 --d_model 256 --layers 4 --drop_rate 0.2 --lr 1e-3 --batch_size 256 --max_epoch 1
+python src/main.py --save --heads 4 --d_model 256 --layers 4 --drop_rate 0.2 --lr 1e-3 --batch_size 256 --max_epoch 1
